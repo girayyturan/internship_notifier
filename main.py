ac5328865112437f6d4c439600e90a47062888cb
girayyturan <girayturan57@gmail.com>
2025-04-03 19:30:10 +0300

    Changes were made in main.py

--- main.py
+++ main.py
@@ -6,19 +6,16 @@ from email.mime.text import MIMEText
 from dotenv import load_dotenv
 import os
 
-load_dotenv() #.env dosyasını yükle
+load_dotenv()
 
-# Youthall Talent Programs Sayfası URL'si
 url = "https://www.youthall.com/tr/talent-programs/?page=1&order=6"
 
-# HTTP isteği gönder
-headers = {"User-Agent": "Mozilla/5.0"}  # Bot olarak algılanmamak için
+headers = {"User-Agent": "Mozilla/5.0"}
 response = requests.get(url, headers=headers)
 
-# E-posta bilgileri
 EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
-RECEIVER_EMAILS = os.getenv("RECEIVER_EMAILS").split(",") # E-posta listesini diziye çevir
+RECEIVER_EMAILS = os.getenv("RECEIVER_EMAILS").split(",")
 
 def send_email(subject, body):
     msg = MIMEMultipart()
@@ -33,32 +30,31 @@ def send_email(subject, body):
         server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         server.sendmail(EMAIL_ADDRESS, RECEIVER_EMAILS, msg.as_string())
         server.quit()
-        print("E-posta başarıyla gönderildi!")
+        print("Mail is sent!")
     except Exception as e:
-        print(f"E-posta gönderilirken hata oluştu: {e}")
+        print(f"An error occured while sending mail {e}")
 
-# Yanıt başarılı mı kontrol et
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, "html.parser")
     job_listings = soup.find_all("div", class_="y-talent box_hover border-line-light-blue shadow-light")
     
-    email_body = "Yeni Staj Programları:\n\n"
+    email_body = "New Internship Programs:\n\n"
     
     for job in job_listings:
         title_tag = job.find("div", class_="y-talent_title")
-        title = title_tag.label.text.strip() if title_tag else "Bilinmiyor"
+        title = title_tag.label.text.strip() if title_tag else "Unknown"
         
         company_tag = job.find("div", class_="y-talent_company_name")
-        company = company_tag.span.text.strip() if company_tag else "Bilinmiyor"
+        company = company_tag.span.text.strip() if company_tag else "Unknown"
         
         link_tag = job.find("a", href=True)
-        link = "https://www.youthall.com" + link_tag["href"] if link_tag else "Bilinmiyor"
+        link = "https://www.youthall.com" + link_tag["href"] if link_tag else "Unknown"
         
-        email_body += f"İlan: {title}\nŞirket: {company}\nBağlantı: {link}\n\n"
+        email_body += f"Job title: {title}\nCompany: {company}\nLink: {link}\n\n"
     
     if job_listings:
-        send_email("Youthall Yeni Staj Programları", email_body)
+        send_email("Youthall New Internship Programs", email_body)
     else:
-        print("Yeni ilan bulunamadı.")
+        print("No new listings found.")
 else:
-    print("Sayfa yüklenemedi!", response.status_code)
+    print("Page could not be loaded!", response.status_code)
